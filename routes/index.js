1cb93bd1323a27e8941a72e0462088f8a922ee5e
Spurrya Jaggi <spurrya@gmail.com>
2016-02-09 19:50:26 -0500

    added routes

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,22 @@
-module.exports = function(app, mongoose){
+module.exports = function(router, mongoose){
   var User = require('../models/User');
-  //Home page
-  app.get('/', function (req, res) {
-    res.send('Hello World!');
+
+  // get an instance of the express Router
+  router.get('/', function(req, res) {
+      res.json({ message: 'hooray! welcome to our api!' });
   });
 
-  //Get users
-  app.get('/user', function(req, res) {
+  // get an instance of the express Router
+  router.get('/users', function(req, res) {
     User.find(function(err, users) {
-      res.send(users);
+      res.json({message: users});
     });
   });
+
+  // middleware to use for all requests
+  router.use(function(req, res, next) {
+      console.log('Something is happening.');
+      next(); // make sure we go to the next routes and don't stop here
+  });
+
 };
--- server.js
+++ server.js
@@ -2,29 +2,19 @@ var express = require('express');
 var mongoose = require('mongoose');
 var config = require('./config.json');
 var bodyParser = require('body-parser');
-var User = require('models/User')
 var app = express();
 mongoose.connect('mongodb://' + config.test)
-
-//Including all the routes
-require('./routes/index')(app, mongoose);
-
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 var port = process.env.PORT || 8080;
 
-var router = express.Router();              // get an instance of the express Router
-router.get('/', function(req, res) {
-    res.json({ message: 'hooray! welcome to our api!' });
-});
+var router = express.Router();
 
-// middleware to use for all requests
-router.use(function(req, res, next) {
-    console.log('Something is happening.');
-    next(); // make sure we go to the next routes and don't stop here
-});
+//Including all the routes
+require('./routes/index')(router, mongoose);
+
 
 // REGISTER OUR ROUTES -------------------------------
 // all of our routes will be prefixed with /api
