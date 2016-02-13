a12fbb16ba65f9168c14beb09c40168e675d2ab0
Spurrya Jaggi <spurrya@gmail.com>
2016-02-13 10:24:39 -0500

    fixed a few stuff

--- routes/index.js
+++ routes/index.js
@@ -1,64 +1,69 @@
 module.exports = function(router, mongoose){
   var User = require('../models/User');
 
-  //Testing welcome message
-  router.get('/', function(req, res) {
-      res.json({ message: 'hooray! welcome to our api!' });
+//Testing welcome message
+router.get('/', function(req, res) {
+    res.json({ message: 'hooray! welcome to our api!' });
+});
+
+//Get list of all the users from the database
+router.get('/users/', function(req, res) {
+  User.find(function(err, users) {
+    res.json({users: users});
   });
-
-  //Get list of all the users from the database
-  router.get('/users/', function(req, res) {
-    User.find(function(err, users) {
-      res.json({users: users});
-    });
-  });
+});
 
 
-
-  router.param('user_id', function(req, res, next, id) {
-    next();
-  });
+router.param('user_id', function(req, res, next, id) {
+  next();
+});
 
 router.route('/users')
 // create a user accessed at POST http://localhost:8080/api/users)
 .post(function(req, res) {
+  console.log(req.body);
     var user = new User({
-      name :  req.body.name,
-      email : req.body.email,
+      name : req.body.name,
+      email :req.body.email,
       chromeId : req.body.chromeId
     });
-
-  //user.save(user);
   user.save(function(err) {
       if (err)
           res.send(err);
-
       res.json({ message: 'User created!' });
   });
 });
-
   // Getting, setting and deleting users
-  router.route('/users/:user_id')
-  .all(function(req, res, next) {
+router.route('/users/:user_id')
+.all(function(req, res, next) {
     console.log("User route");
     next();
-  })
+})
   //Finds the user by id and returns that user
-  .get(function(req, res, next) {
+.get(function(req, res, next) {
     User.findById(req.params.user_id, function(err, user) {
     if (err)
         res.send(err);
     res.json({user: user});
   })
-  })
-  .put(function(req, res, next) {
+})
+.put(function(req, res, next) {
     // just an example of maybe updating the user
     req.user.name = req.params.name;
     // save user ... etc
     res.json(req.user);
-  })
-  .delete(function(req, res, next) {
-    next(new Error('not implemented'));
+})
+.delete(function(req, res) {
+    // delete the user with this id (accessed at DELETE http://localhost:8080/api/bears/:user_id)
+
+  User.remove({
+      _id: req.params.user_id
+  }, function(err, bear) {
+      if (err)
+          res.send(err);
+
+      res.json({ message: 'Successfully deleted' });
   });
+});
 
 };
