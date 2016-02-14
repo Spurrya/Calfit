d9c59739137dd860216bfac03cc93b8640ff940a
Spurrya Jaggi <spurrya@gmail.com>
2016-02-14 17:10:41 -0500

    found the reason why it didnt work on deployment end

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 var express = require('express');
 var mongoose = require('mongoose');
-//var config = require('./config');
+var config = require('./config');
 var bodyParser = require('body-parser');
-//var auth = require('./auth');
-//var graph = require('./graph');
+var auth = require('./auth');
+var graph = require('./graph');
 
 
 var app = express();
 
-mongoose.connect('mongodb://user1:user1@ds059145.mongolab.com:59145/yofitdb');
+mongoose.connect(config.hostedDatabase);
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
@@ -23,21 +23,21 @@ console.log('Magic happens on port ' + port);
 
 
 // Get an access token for the app.
-// auth.getAccessToken().then(function (token) {
-//   // Get all of the users in the tenant.
-//   graph.getUsers(token)
-//     .then(function (users) {
-//       // Create an event on each user's calendar.
-//       //Uncomment when you want to create a new event in the API
-//       //graph.createEvent(token, users);
-//     }, function (error) {
-//       console.error('>>> Error getting users: ' + error);
-//     }).then(function (users) {
-//       // Get calendar events for users
-//       graph.getEvents(token, users);
-//     }, function (error) {
-//       console.error('>>> Error getting calendar events for users: ' + error);
-//     });
-// }, function (error) {
-//   console.error('>>> Error getting access token: ' + error);
-// });
+auth.getAccessToken().then(function (token) {
+  // Get all of the users in the tenant.
+  graph.getUsers(token)
+    .then(function (users) {
+      // Create an event on each user's calendar.
+      //Uncomment when you want to create a new event in the API
+      //graph.createEvent(token, users);
+    }, function (error) {
+      console.error('>>> Error getting users: ' + error);
+    }).then(function (users) {
+      // Get calendar events for users
+      graph.getEvents(token, users);
+    }, function (error) {
+      console.error('>>> Error getting calendar events for users: ' + error);
+    });
+}, function (error) {
+  console.error('>>> Error getting access token: ' + error);
+});
