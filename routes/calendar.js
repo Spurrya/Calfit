3f58a03863806ed6eb056e73073ad7c01e982d09
Spurrya Jaggi <spurrya@gmail.com>
2016-02-14 18:33:23 -0500

    added a post method, currently creates a hardcoded event

--- routes/calendar.js
+++ routes/calendar.js
@@ -7,12 +7,6 @@ module.exports = function(router, mongoose, auth, graph){
       // Get all of the users in the tenant.
       graph.getUsers(token)
         .then(function (users) {
-          // Create an event on each user's calendar.
-          //Uncomment when you want to create a new event in the API
-          //graph.createEvent(token, users);
-        }, function (error) {
-          console.error('>>> Error getting users: ' + error);
-        }).then(function (users) {
           // Get calendar events for users
           graph.getEvents(token, users,res);
         }, function (error) {
@@ -21,6 +15,26 @@ module.exports = function(router, mongoose, auth, graph){
     }, function (error) {
       console.error('>>> Error getting access token: ' + error);
     });
-  })
+  });
+
+
+  router.route('/calendar')
+  // create a user accessed at POST http://localhost:8080/api//calendar)
+  .post(function(req, res) {
+    // Get an access token for the app.
+    auth.getAccessToken().then(function (token) {
+      // Get all of the users in the tenant.
+      graph.getUsers(token)
+        .then(function (users) {
+          // Create an event on each user's calendar.
+          graph.createEvent(token, users);
+        }, function (error) {
+          console.error('>>> Error getting users: ' + error);
+        })
+    }, function (error) {
+      console.error('>>> Error getting access token: ' + error);
+    }
+  );
+  });
 
 }
