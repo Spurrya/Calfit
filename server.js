91af9fd67b30238a199bbc6772a6837b91ad74e0
Spurrya Jaggi <spurrya@gmail.com>
2016-02-13 23:33:10 -0500

    quick fix

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 var express = require('express');
 var mongoose = require('mongoose');
-var config = require('./config.json');
+//var config = require('./config.json');
 var bodyParser = require('body-parser');
 var authContext = require('adal-node').AuthenticationContext;
 var request = require('ajax-request');
-var authHelper = require('./authHelper.js');
-var requestUtil = require('./requestUtil.js')
+//var authHelper = require('./authHelper.js');
+//var requestUtil = require('./requestUtil.js')
 var cookieParser = require('cookie-parser');
 var session = require('express-session')
 var logger = require('morgan');
@@ -28,7 +28,7 @@ var port = process.env.PORT || 3000;
 var router = express.Router();
 
 require('./routes/index')(router, mongoose);
-require('./routes/calendar')(router,mongoose, authHelper, requestUtil);
+//require('./routes/calendar')(router,mongoose, authHelper, requestUtil);
 
 
 app.use('/api', router);
