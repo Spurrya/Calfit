6e499baa25f36e35d7ff4e584f2fb43daba1f3cf
Spurrya Jaggi <spurrya@gmail.com>
2016-02-15 23:19:57 -0500

    added the old code

--- chrome-extension/register.js
+++ chrome-extension/register.js
@@ -32,7 +32,62 @@ function registerCallback(regId) {
   }
   chrome.storage.local.set({registered: true});
   registerofficeuser(regId)
-}
+ }
+
+
+ // OLD CODE FOR REFERENCE
+// var registrationId = "";
+//
+// function setStatus(status) {
+//   document.getElementById("status").innerHTML = status;
+// }
+//
+// function register() {
+//   var senderId = document.getElementById("senderId").value;
+//   chrome.gcm.register([senderId], registerCallback);
+//
+//   setStatus("Registering ...");
+//
+//   // Prevent register button from being click again before the registration
+//   // finishes.
+//   document.getElementById("register").disabled = true;
+// }
+//
+// function registerofficeuser() {
+// 	setStatus("Registering user....");
+// 	var email = document.getElementById("email").value;
+// 	var name = document.getElementById("name").value;
+// 	var xhttp = new XMLHttpRequest();
+// 	xhttp.onreadystatechange = function() {
+// 		setStatus("User details has successfully sent....");
+// 		if (xhttp.status == 200) {
+// 		  //TBD
+// 		}
+// 	};
+// 	xhttp.open("POST", "http://calfit.azurewebsites.net/api/users", true);
+// 	xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+// 	xhttp.send("email="+ varRegisteruserid + "&chromeId=" + registrationId + "&name=" + varNameId );
+// }
+//
+// function registerCallback(regId) {
+//   registrationId = regId;
+//   document.getElementById("register").disabled = false;
+// document.getElementById("regId").innerHTML = "Registration ID : " + regId;
+//   if (chrome.runtime.lastError) {
+//     // When the registration fails, handle the error and retry the
+//     // registration later.
+//     setStatus("Registration failed: " + chrome.runtime.lastError.message);
+//     return;
+//   }
+//
+//   setStatus("Registration succeeded. Please run the following command to send a message.");
+//
+//   // Mark that the first-time registration is done.
+//   chrome.storage.local.set({registered: true});
+//
+//   // Format and show the curl command that can be used to post a message.
+//   updateCurlCommand();
+// }
 //
 // function updateCurlCommand() {
 //   var apiKey = document.getElementById("apiKey").value;
@@ -70,4 +125,3 @@ function registerCallback(regId) {
 //   document.getElementById("msgKey").onchange = updateCurlCommand;
 //   document.getElementById("msgValue").onchange = updateCurlCommand;
 //   setStatus("You have not registered yet. Please provider sender ID and register.");
-// }
