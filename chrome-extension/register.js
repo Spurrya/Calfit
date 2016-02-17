78d0544ccc6af0646926db6b95b9d0e177ca4a69
Spurrya Jaggi <spurrya@gmail.com>
2016-02-16 20:38:20 -0500

    Restructured callback function

--- chrome-extension/register.js
+++ chrome-extension/register.js
@@ -1,43 +1,37 @@
-registrationId = ""
-
-function register() {
+function register(callback) {
   var senderId = '597929500512';
-  chrome.gcm.register([senderId], registerCallback);
+  chrome.gcm.register([senderId], function (regId) {
+      if (chrome.runtime.lastError) {
+        return false;
+      }
+      chrome.storage.local.set({registered: true});
+      callback(regId);
+  });
 }
-
-
-function registerCallback(regId) {
-  registrationId = regId;
-  if (chrome.runtime.lastError) {
-    return false;
-  }
-  chrome.storage.local.set({registered: true});
- }
-
 
 $(function(){
   $("#register-form").submit(function(e) {
+    //Disable from further calls
+    $('#submit').disabled = true;
+    register(function (registrationId) {
+      var name = $('#name').val()
+      var email = $('#email').val()
+      //Insert console.log or alert here to slow it down
+      var chromeId = registrationId
 
-    register()
-    var name = $('#name').val()
-    var email = $('#email').val()
-    console.log('f');
-    var chromeId = registrationId
-
-    $.ajax({
-         type: "POST",
-         url: 'http://calfit.azurewebsites.net/api/users',
-         ajax:false,
-         data: {chromeId: chromeId, name: name, email:email},
-         success: function(result)
-         {
-           console.log(result)
-         }
-       });
-
+      $.ajax({
+           type: "POST",
+           url: 'http://calfit.azurewebsites.net/api/users',
+           ajax:false,
+           data: {chromeId: chromeId, name: name, email:email},
+           success: function(result)
+           {
+             console.log(result)
+           }
+         });
+    });
   });
 })
-
 
  // OLD CODE FOR REFERENCE
 // var registrationId = "";
