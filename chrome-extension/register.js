47461bf048535f717252643801ae5d82d83fe066
Spurrya Jaggi <spurrya@gmail.com>
2016-02-19 21:06:51 -0500

    need to add sandbox now

--- chrome-extension/register.js
+++ chrome-extension/register.js
@@ -10,25 +10,71 @@ function register(callback) {
 }
 
 $(function(){
-  $("#register-form").submit(function(e) {
-    //Disable from further calls
-    $('#submit').disabled = true;
-    register(function (registrationId) {
-      var name = $('#name').val()
-      var email = $('#email').val()
-      //Insert console.log or alert here to slow it down
-      var chromeId = registrationId
+  // $("#register-form").submit(function(e) {
+  //   //Disable from further calls
+  //   $('#submit').disabled = true;
+  //   register(function (registrationId) {
+  //     var name = $('#name').val()
+  //     var email = $('#email').val()
+  //     //Insert console.log or alert here to slow it down
+  //     var chromeId = registrationId
+  //
+  //     $.ajax({
+  //          type: "POST",
+  //          url: 'http://calfit.azurewebsites.net/api/users',
+  //          ajax:false,
+  //          data: {chromeId: chromeId, name: name, email:email},
+  //          success: function(result)
+  //          {
+  //            $('#success').show()
+  //          }
+  //        });
+  //   });
+  // });
+  $('#signin').click(function() {
+  	WL.init({
+  		client_id: "5d062d99-e648-4238-a13a-16200788a1b6",
+  		redirect_uri: "https://login.live.com/oauth20_desktop.srf",
+  		response_type: "token"
+  	});
+  	WL.login({
+          scope: ["wl.signin", "office.calendar"]
+      });
 
-      $.ajax({
-           type: "POST",
-           url: 'http://calfit.azurewebsites.net/api/users',
-           ajax:false,
-           data: {chromeId: chromeId, name: name, email:email},
-           success: function(result)
-           {
-             $('#success').show()
-           }
-         });
-    });
+  	return false;
+
   });
+
+if (window.location.origin == "https://login.live.com") {
+  var hash = window.location.hash;
+  // get access token
+  var start = hash.indexOf("#access_token=");
+  if ( start >= 0 ) {
+    start = start + "#access_token=".length;
+
+    var end = hash.indexOf("&token_type");
+    var access_token = hash.substring(start, end);
+
+    // Store it
+    chrome.storage.local.set({"access_token":access_token});
+
+    register(function (registrationId) {
+      var chromeId = registrationId
+      var officeId = access_token
+        $.ajax({
+             type: "POST",
+             url: 'http://calfit.azurewebsites.net/api/users',
+             data: {chromeId: chromeId, officeId: officeId},
+             success: function(result)
+             {
+               $('#success').show()
+             }
+           });
+      });
+
+    // Close the window
+    window.close();
+  }
+}
+
 })
